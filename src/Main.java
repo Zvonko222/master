5d1dc8c3e8673b0265e11effdaae6f04740b4581
Zvonko <hyjkgyk@gmail.com>
2026-06-15 17:06:29 +0800

    aaa

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         BaseApplication app = new LibraryApplication();
-
+//
         app.onCreate();
         app.run();
         app.onDestroy();
